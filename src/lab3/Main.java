c906b461631519100b752dbb84b9e4f36f13cf01
Vladyslav Udod <volodkatraktorist@gmail.com>
2024-12-13 17:00:33 +0200

    adds lab3 SeaShip sort and search

--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -1,7 +1,45 @@
 package lab3;
+
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 //C11 = 3 - Визначити клас морський човен, який складається як мінімум з 5-и полів.
 public class Main {
     public static void main(String[] args) {
+        SeaShip[] shipsArray = {
+                new SeaShip(1L, "Titanic", 9, 2400, 23.0),
+                new SeaShip(2L, "Queen Mary 2", 13, 2060, 30.0),
+                new SeaShip(3L, "Allure of the Seas", 16, 5400, 22.0),
+                new SeaShip(4L, "USS Enterprise", 20, 5000, 33.6),
+                new SeaShip(5L, "Black Pearl", 3, 500, 15.0)
+        };
+        System.out.println("Масив кораблів:");
+        printArray(shipsArray);
+        SeaShip[] sortedBySpeedShips = Stream.of(shipsArray)
+                .sorted(Comparator.comparing(SeaShip::getSpeed))
+                .toArray(SeaShip[]::new);
+        System.out.println("\nВідсортовані за швидкістю: ");
+        printArray(sortedBySpeedShips);
 
+        SeaShip[] sortedByCapacityReverseShips = Stream.of(shipsArray)
+                .sorted(Comparator.comparing(SeaShip::getCapacity).reversed())
+                .toArray(SeaShip[]::new);
+        System.out.println("\nВідсортовані за місткістю в зворотньому напрямку:");
+        printArray(sortedByCapacityReverseShips);
+
+        SeaShip shipToFind = new SeaShip(6L, "Titanic", 9, 2400, 23.0);
+        System.out.println("\nОб'єкт для пошуку:\n" + shipToFind);
+        for (SeaShip ship : shipsArray) {
+            if (ship.equals(shipToFind)) {
+                System.out.println("\nІдентичний об'єкт:\n" + ship);
+            }
+        }
+    }
+
+    private static void printArray(SeaShip[] ships) {
+        for (SeaShip ship : ships) {
+            System.out.println(ship);
+        }
     }
 }
--- src/lab3/SeaShip.java
+++ src/lab3/SeaShip.java
@@ -1,16 +1,18 @@
 package lab3;
 
 
+import java.util.Objects;
+
 public class SeaShip {
-    private final Double height;
-    private final Double length;
+    private final Long id;
+    private final String name;
     private final Integer floors;
     private final Integer capacity;
     private final Double speed;
 
-    public SeaShip(Double height, Double length, Integer floors, Integer capacity, Double speed) {
-        this.height = height;
-        this.length = length;
+    public SeaShip(Long id, String name, Integer floors, Integer capacity, Double speed) {
+        this.id = id;
+        this.name = name;
         this.floors = floors;
         this.capacity = capacity;
         this.speed = speed;
@@ -18,12 +20,37 @@ public class SeaShip {
 
     @Override
     public String toString() {
-        return "SeaShip{" +
-                "height=" + height + " m." +
-                ", length=" + length + " m." +
-                ", floors=" + floors + " floors" +
-                ", capacity=" + capacity + " people" +
-                ", speed=" + speed + " knots" +
-                '}';
+        return "Id: " + id +
+                ", Name: " + name +
+                ", Floors: " + floors + " floors" +
+                ", Capacity: " + capacity + " people" +
+                ", Speed: " + speed + " knots";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getFloors() {
+        return floors;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SeaShip seaShip = (SeaShip) o;
+        return Objects.equals(name, seaShip.name) && Objects.equals(floors, seaShip.floors) && Objects.equals(capacity, seaShip.capacity) && Objects.equals(speed, seaShip.speed);
     }
 }
